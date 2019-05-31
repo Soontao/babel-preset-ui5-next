d80e20518f568b2b978938861162acb9d8813796
Theo Sun <theo.sun@sap.com>
2019-05-31 11:21:56 +0800

    fix(regenerator): lost

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ module.exports = function preset(context, options = {}) {
       "babel-plugin-transform-es2015-destructuring",
       "babel-plugin-transform-es2015-block-scoping",
       "babel-plugin-transform-es2015-typeof-symbol",
-      "babel-plugin-transform-regenerator",
       "babel-plugin-transform-object-assign",
+      "babel-plugin-transform-async-await",
       "babel-plugin-transform-object-rest-spread",
       ["babel-plugin-ui5-next", { namespace }],
       "babel-plugin-transform-es2015-classes"
