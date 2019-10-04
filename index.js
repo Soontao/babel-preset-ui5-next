69f13336a18e8e59f7643e5b57bb8680467c12e0
Theo Sun <theo.sun@sap.com>
2019-10-04 13:14:12 +0800

    feat: support babel 7

--- index.js
+++ index.js
@@ -8,32 +8,39 @@ module.exports = function preset(context, options = {}) {
 
   return {
     plugins: [
-      "babel-plugin-syntax-jsx",
-      "babel-plugin-syntax-decorators",
-      "babel-plugin-syntax-class-properties",
-      "babel-plugin-transform-es2015-template-literals",
-      "babel-plugin-transform-es2015-literals",
-      "babel-plugin-transform-es2015-function-name",
-      "babel-plugin-transform-es2015-arrow-functions",
-      "babel-plugin-transform-es2015-block-scoped-functions",
-      "babel-plugin-transform-es2015-shorthand-properties",
-      "babel-plugin-transform-es2015-computed-properties",
-      "babel-plugin-transform-es2015-duplicate-keys",
-      "babel-plugin-transform-es2015-for-of",
-      "babel-plugin-transform-es2015-sticky-regex",
-      "babel-plugin-transform-es2015-unicode-regex",
-      "babel-plugin-check-es2015-constants",
-      "babel-plugin-transform-es2015-spread",
-      "babel-plugin-transform-es2015-parameters",
-      "babel-plugin-transform-es2015-destructuring",
-      "babel-plugin-transform-es2015-block-scoping",
-      "babel-plugin-transform-es2015-typeof-symbol",
-      "babel-plugin-transform-object-assign",
-      "babel-plugin-transform-async-await",
-      "babel-plugin-transform-object-rest-spread",
-      ["babel-plugin-ui5-next", { namespace }],
-      "babel-plugin-transform-es2015-classes",
-      "babel-plugin-transform-react-jsx"
+      [
+        "babel-plugin-ui5-next", { namespace }
+      ],
+      "@babel/plugin-syntax-jsx",
+      "@babel/plugin-syntax-class-properties",
+      "babel-plugin-transform-async-to-promises",
+      "@babel/plugin-transform-template-literals",
+      "@babel/plugin-transform-literals",
+      "@babel/plugin-transform-function-name",
+      "@babel/plugin-transform-arrow-functions",
+      "@babel/plugin-transform-block-scoped-functions",
+      "@babel/plugin-transform-shorthand-properties",
+      "@babel/plugin-transform-computed-properties",
+      "@babel/plugin-transform-duplicate-keys",
+      "@babel/plugin-transform-for-of",
+      "@babel/plugin-transform-sticky-regex",
+      "@babel/plugin-transform-unicode-regex",
+      "@babel/plugin-check-constants",
+      "@babel/plugin-transform-spread",
+      "@babel/plugin-transform-parameters",
+      "@babel/plugin-transform-destructuring",
+      "@babel/plugin-transform-block-scoping",
+      "@babel/plugin-transform-typeof-symbol",
+      "@babel/plugin-transform-regenerator",
+      "@babel/plugin-transform-object-assign",
+      "@babel/plugin-proposal-object-rest-spread",
+      [
+        "@babel/plugin-proposal-decorators",
+        {
+          "legacy": true
+        }
+      ],
+      "@babel/plugin-transform-react-jsx"
     ]
   };
 
